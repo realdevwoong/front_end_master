b69fa38d38b109e30074159733a5d13076b25596
realdevwoong <projung134@gmail.com>
2025-06-25 22:57:40 +0900

    start

--- woo/frontend/js/main.js
+++ woo/frontend/js/main.js
@@ -14,5 +14,18 @@ searchInputEl.addEventListener('blur', function(){
 
 const badgeEl = document.querySelector('header .badges');
 window.addEventListener('scroll', _.throttle(function(){
-  console.log('scroll!!');
+  console.log(window.scorllY)
+  if(window.scrollY>500){
+    // gsap.to(요소, 지속시간(), 옵션);
+    gsap.to(badgeEl, .6, {
+      opacity:0,
+      display:'none'
+    })
+  }else{
+    //배지보이기 
+    gsap.to(badgeEl, .6, {
+      opacity:1,
+      display:'block'
+    });
+  }
 },300));
